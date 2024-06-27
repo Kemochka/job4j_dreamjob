3e0b0a6eb1587dd6494718242e2873720e8149da
kemochka <kemaevak@yandex.ru>
2024-06-27 15:22:19 +0300

    2. Связь слоев через Spring DI. [#504856]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -1,5 +1,6 @@
 package ru.job4j.dreamjob.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.Candidate;
 
 import java.time.LocalDateTime;
@@ -8,8 +9,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+@Repository
 public class MemoryCandidateRepository implements CandidateRepository {
-    private static final MemoryCandidateRepository INSTANCE = new MemoryCandidateRepository();
     private int nextId = 1;
     private final Map<Integer, Candidate> candidates = new HashMap<>();
 
@@ -23,9 +24,6 @@ public class MemoryCandidateRepository implements CandidateRepository {
 
     }
 
-    public static MemoryCandidateRepository getInstance() {
-        return INSTANCE;
-    }
     @Override
     public Candidate save(Candidate candidate) {
         candidate.setId(nextId++);
--- src/main/java/ru/job4j/dreamjob/service/SimpleCandidateService.java
+++ src/main/java/ru/job4j/dreamjob/service/SimpleCandidateService.java
@@ -1,43 +1,43 @@
 package ru.job4j.dreamjob.service;
 
+import org.springframework.stereotype.Service;
 import ru.job4j.dreamjob.model.Candidate;
+import ru.job4j.dreamjob.repository.CandidateRepository;
 
 import java.util.Collection;
 import java.util.Optional;
 
+ @Service
 public class SimpleCandidateService implements CandidateService {
-    private static final SimpleCandidateService INSTANCE = new SimpleCandidateService();
-    private final CandidateService candidateService = SimpleCandidateService.getInstance();
+    private final CandidateRepository candidateRepository;
 
-    public SimpleCandidateService() {
+    public SimpleCandidateService(CandidateRepository candidateRepository) {
+        this.candidateRepository = candidateRepository;
     }
 
-    public static SimpleCandidateService getInstance() {
-        return INSTANCE;
-    }
 
     @Override
     public Candidate save(Candidate candidate) {
-        return candidateService.save(candidate);
+        return candidateRepository.save(candidate);
     }
 
     @Override
     public boolean deleteById(int id) {
-        return candidateService.deleteById(id);
+        return candidateRepository.deleteById(id);
     }
 
     @Override
     public boolean update(Candidate candidate) {
-        return candidateService.update(candidate);
+        return candidateRepository.update(candidate);
     }
 
     @Override
     public Optional<Candidate> findById(int id) {
-        return candidateService.findById(id);
+        return candidateRepository.findById(id);
     }
 
     @Override
     public Collection<Candidate> findAll() {
-        return candidateService.findAll();
+        return candidateRepository.findAll();
     }
 }
