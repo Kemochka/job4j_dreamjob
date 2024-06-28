cdb0586ee7179dca12e59be9f818954a299d5c48
kemochka <kemaevak@yandex.ru>
2024-06-28 13:47:00 +0300

    1. Формы. Поля ввода. [#504853]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -20,12 +20,12 @@ public class MemoryVacancyRepository implements VacancyRepository {
     private final Map<Integer, Vacancy> vacancies = new ConcurrentHashMap<>();
 
     private MemoryVacancyRepository() {
-        save(new Vacancy(0, "Intern Java Developer", "Intern", LocalDateTime.now()));
-        save(new Vacancy(0, "Junior Java Developer", "Junior", LocalDateTime.now()));
-        save(new Vacancy(0, "Junior+ Java Developer", "Junior+", LocalDateTime.now()));
-        save(new Vacancy(0, "Middle Java Developer", "Middle", LocalDateTime.now()));
-        save(new Vacancy(0, "Middle+ Java Developer", "Middle+", LocalDateTime.now()));
-        save(new Vacancy(0, "Senior Java Developer", "Senior", LocalDateTime.now()));
+        save(new Vacancy(0, "Intern Java Developer", "Intern", LocalDateTime.now(), false));
+        save(new Vacancy(0, "Junior Java Developer", "Junior", LocalDateTime.now(), false));
+        save(new Vacancy(0, "Junior+ Java Developer", "Junior+", LocalDateTime.now(), false));
+        save(new Vacancy(0, "Middle Java Developer", "Middle", LocalDateTime.now(), false));
+        save(new Vacancy(0, "Middle+ Java Developer", "Middle+", LocalDateTime.now(), true));
+        save(new Vacancy(0, "Senior Java Developer", "Senior", LocalDateTime.now(), false));
     }
 
     @Override
@@ -44,7 +44,7 @@ public class MemoryVacancyRepository implements VacancyRepository {
     public boolean update(Vacancy vacancy) {
         return vacancies.computeIfPresent(vacancy.getId(),
                 (id, oldVacancy) -> new Vacancy(oldVacancy.getId(), vacancy.getTitle(),
-                        vacancy.getDescription(), vacancy.getCreationDate())) != null;
+                        vacancy.getDescription(), vacancy.getCreationDate(), vacancy.getVisible())) != null;
     }
 
     @Override
